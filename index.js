d012a2e96959c9aa1e7934fefeb813ccab597b47
adnapp <adam.napps@gmail.com>
2021-03-18 12:28:57 -0400

    commit tree 855e14ceeaaf7a33b79e3a49c7a0bcd1a4fb2519

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(arr, callback){
+    let r=[]
+    for (let i=0; i<arr.length; i++){
+        r.push(callback(arr[i]))
+    }
+    return r
+}
+
+
+function reduce(arr, callback, starting){
+    let reduced = (!!starting? starting : 0)
+
+    for (let i=0; i<arr.length; i++){
+        reduced = callback(arr[i], reduced)
+    }
+
+
+    return reduced
+
+}
